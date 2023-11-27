23b518614911e49de1ef3b8f777c08ed3f96a888
KoNyaShKa <ggiziawpgg@gmail.com>
2023-11-27 13:35:05 +0300

    ?

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 'use strict';
 
+//nulish - стандарт ES11
+//const box = document.querySelector('.box');
+//
+//const newHeight = 100;
+//const newWidth = 400;
+//
+//function changeParams(elem, h, w) {
+//    elem.style.height = `${h ?? 200}px`;//?? - Данный знак означает или но только в случае 0 или undefined, то есть даже ноль тут будет отображаться
+//    elem.style.width = `${w ?? 200}px`;
+//}
+//
+//changeParams(box, newHeight, newWidth);
+
 const box = document.querySelector('.box');
+const block = document.querySelector('.block');
 
-const newHeight = 100;
-const newWidth = 400;
 
-function changeParams(elem, h, w) {
-    elem.style.height = `${h ?? 200}px`;//?? - Данный знак означает или но только в случае 0 или undefined, то есть даже ноль тут будет отображаться
-    elem.style.width = `${w ?? 200}px`;
-}
+console.log(block)
 
-changeParams(box, newHeight, newWidth);
+console.log(block?.textContent);//если нет значения, не будет вылазить ошибка, а просто undefined. Работает только на чтение свойства
+
+console.log(1+2);
